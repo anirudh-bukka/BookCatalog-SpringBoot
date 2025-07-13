af19c23ee14fc450b09090d212bdf99cd7ac72cc
anirudh-bukka <anirudh.bukka@gmail.com>
2025-07-13 19:13:53 +0530

    Created jpa repository for review class

--- src/main/java/com/anirudh/bookstore/demo/dao/ReviewRepository.java
+++ src/main/java/com/anirudh/bookstore/demo/dao/ReviewRepository.java
@@ -3,5 +3,7 @@ package com.anirudh.bookstore.demo.dao;
 import com.anirudh.bookstore.demo.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 }
