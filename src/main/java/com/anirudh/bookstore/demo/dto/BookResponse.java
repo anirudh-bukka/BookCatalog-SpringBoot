6521d8eaca48f373a105d20b342b41297b49acca
anirudh-bukka <anirudh.bukka@gmail.com>
2025-07-13 19:14:27 +0530

    created dto for book response

--- src/main/java/com/anirudh/bookstore/demo/dto/BookResponse.java
+++ src/main/java/com/anirudh/bookstore/demo/dto/BookResponse.java
@@ -1,4 +1,61 @@
 package com.anirudh.bookstore.demo.dto;
 
+import java.util.List;
+
 public class BookResponse {
+    private int id;
+    private String title;
+    private int price;
+    private String genre;
+    private int quantity;
+    private List<String> reviews; // Only review texts
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public List<String> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<String> reviews) {
+        this.reviews = reviews;
+    }
 }
